3294d9ed2e15fdeb240100ffaf9b58e6caf9ce69
Concinnity Digital <concinnity.digital@gmail.com>
2023-12-04 20:09:59 +0000

    added fast_fleet_sales 2023-12

--- models/fast_fleet/marts/fast_fleet_sales.sql
+++ models/fast_fleet/marts/fast_fleet_sales.sql
@@ -206,6 +206,24 @@ unioned as (
         Notes,
         Shift
     from {{ ref('stg_fast_fleet_sales__2023_11') }}
+    union all
+    select 
+        Date,
+        Customer,
+        Amount,
+        Labor_Cost,
+        Job_Supplies,
+        Gross_Revenue,
+        GP_Margin,
+        Service_Type,
+        Classification,
+        Job,
+        Payment_Method,
+        Truck,    
+        Lead_Source,    
+        Notes,
+        Shift
+    from {{ ref('stg_fast_fleet_sales__2023_12') }}
 ),
 add_row_num as (
     select 
