3e293436303fbc2221cdba70e836f59114dff576
Concinnity Digital <concinnity.digital@gmail.com>
2024-02-07 19:09:11 +0000

    Jan-Pro: added Search Terms Fast Fleet: added Feb 2024  sales sql

---
+++ models/jan_pro/marts/jan_pro_searchTerms.sql
@@ -0,0 +1,15 @@
+with customers as (
+SELECT 
+  customer_id,
+  customer_descriptive_name
+FROM {{ ref('stg_jan_pro_detroit_googleads__ads_Customer') }}
+group by 1,2
+)
+
+SELECT
+  search_terms.*,
+  customers.customer_descriptive_name
+FROM {{ ref('stg_jan_pro_googleads__ads_SearchQueryStats') }} as search_terms
+LEFT JOIN customers
+  on customers.customer_id= search_terms.customer_id
+where customers.customer_descriptive_name = "JAN-PRO Detroit"
