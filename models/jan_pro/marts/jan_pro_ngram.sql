792bd4ce79f24326bd869c3ef382ebc74c550617
Concinnity Digital <concinnity.digital@gmail.com>
2024-02-19 20:30:46 +0000

    Jan-Pro: added Chicago account location

--- models/jan_pro/marts/jan_pro_ngram.sql
+++ models/jan_pro/marts/jan_pro_ngram.sql
@@ -14,4 +14,3 @@ SELECT
 FROM {{ ref('stg_jan_pro_detroit_googleads__ads_SearchQueryStats') }} as search_terms
 LEFT JOIN customers
   on customers.customer_id= search_terms.customer_id
-where customers.customer_descriptive_name = "JAN-PRO Detroit"
