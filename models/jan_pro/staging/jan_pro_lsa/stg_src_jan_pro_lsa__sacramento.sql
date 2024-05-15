c57bafb594b4270d6019ffbdeb46dd58d1b1c50f
Concinnity Digital <concinnity.digital@gmail.com>
2024-05-15 06:54:16 +0000

    Jan Pro: added other LSA locations

---
+++ models/jan_pro/staging/jan_pro_lsa/stg_src_jan_pro_lsa__sacramento.sql
@@ -0,0 +1,34 @@
+{{ config(
+    materialized='table'
+) }}
+
+
+with source as (
+    select 
+        *,
+        trim(split(date_received, ' at')[0]) as date1,
+        trim(split(date_received, ' at')[1]) as time,
+        "JP Sacramento" as location
+    from {{ source('src_jan_pro_lsa', 'sacramento') }}
+),
+
+renamed as (
+    select
+        PARSE_DATE('%B %d, %Y', CONCAT(SUBSTRING(date1, 1, INSTR(date1, ' ') - 1), ' ', SUBSTRING(date1, INSTR(date1, ' ') + 1, INSTR(date1, ',') - INSTR(date1, ' ') - 1), ', ', RIGHT(date1, 4))) AS date,
+        time,
+        date_received,
+        cast(lead_id as string) as lead_id,
+        lead_type,
+        charge_status,
+        name_number,
+        notes,
+        intent_of_the_call,
+        dispute,
+        dispute_status,
+        remarks,
+        location
+    from source
+    where date_received is not null
+)
+
+select * from renamed
