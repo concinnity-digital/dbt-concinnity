47a5e47000f5d986f7aa11d920ef405ef23f6cb7
Concinnity Digital <concinnity.digital@gmail.com>
2024-08-14 18:49:01 +0000

    JanPro: updated LSA Raleigh and Pittsburgh

--- models/jan_pro/staging/jan_pro_lsa/stg_src_jan_pro_lsa__pittsburgh.sql
+++ models/jan_pro/staging/jan_pro_lsa/stg_src_jan_pro_lsa__pittsburgh.sql
@@ -6,8 +6,8 @@
 with source as (
     select 
         *,
-        trim(split(date_received, ' at')[0]) as date1,
-        trim(split(date_received, ' at')[1]) as time,
+        trim(split(string_field_0, ' at')[0]) as date1,
+        trim(split(string_field_0, ' at')[1]) as time,
         "JP Pittsburgh" as location
     from {{ source('src_jan_pro_lsa', 'pittsburgh') }}
 ),
@@ -16,19 +16,19 @@ renamed as (
     select
         PARSE_DATE('%B %d, %Y', CONCAT(SUBSTRING(date1, 1, INSTR(date1, ' ') - 1), ' ', SUBSTRING(date1, INSTR(date1, ' ') + 1, INSTR(date1, ',') - INSTR(date1, ' ') - 1), ', ', RIGHT(date1, 4))) AS date,
         time,
-        date_received,
-        lead_id,
-        lead_type,
-        charge_status,
-        name_number,
-        notes,
-        intent_of_the_call,
-        cast(dispute as string) as dispute,
-        dispute_status,
-        remarks,
+        string_field_0 as date_received,
+        string_field_1 as lead_id,
+        string_field_2 as lead_type,
+        string_field_3 as charge_status,
+        string_field_4 as name_number,
+        string_field_5 as notes,
+        string_field_6 as intent_of_the_call,
+        cast(string_field_7 as string) as dispute,
+        string_field_8 as dispute_status,
+        string_field_9 as remarks,
         location
     from source
-    where date_received is not null
+    where string_field_0 is not null
 )
 
 select * from renamed
