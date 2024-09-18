9075c7b757fb9cab726ae7d7247fd4894bd24437
Concinnity Digital <concinnity.digital@gmail.com>
2024-09-18 18:03:07 +0000

    Jan-Pro: updated Inside Sales Appointments, added new columns

---
+++ models/jan_pro/staging/src_jan_pro_inside_sales_appointments/stg_src_jan_pro_inside_sales_appointments__appointments_nashville.sql
@@ -0,0 +1,42 @@
+with 
+
+source as (
+
+    select * from {{ source('src_jan_pro_inside_sales_appointments', 'appointments_nashville') }}
+
+),
+
+renamed as (
+
+    select
+        cast(timestamp as string) as timestamp,
+        email_address,
+        type_of_appointment,
+        cast(proposed_date_of_appointment as date) as proposed_date_of_appointment,
+        proposed_time_of_appointment,
+        company_name,
+        company_address,
+        contact_name,
+        company_contact_number,
+        company_email_address,
+        inside_sales_staff,
+        notes,
+        valid_ as valid,
+        proposal_ as proposal,
+        closed_ as closed,
+        date_closed as date_closed,
+        safe_cast(contract_revenue as integer) as contract_rev,
+        appointment_commission,
+        closed_sale_commission,
+        paid_ as paid,
+        cast(null as string) as account,
+        "JP Nashville" as location
+
+    from source
+        where timestamp is not null
+        or notes NOT LIKE '%test%' 
+        or notes NOT LIKE '%TEST%'
+
+    )
+
+select * from renamed
