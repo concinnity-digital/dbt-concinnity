a6b8f8392cb4c4fd206f7b3c9e7d7ffe9a3cebf1
Concinnity Digital <concinnity.digital@gmail.com>
2024-10-09 00:57:45 +0000

    JanPro: create Inside Sales Master contact list

---
+++ models/jan_pro/staging/jan_pro_inside_sales_contactlist/stg_src_jan_pro_inside_sales_contactlist__contacts_knoxville.sql
@@ -0,0 +1,39 @@
+with 
+
+source as (
+
+    select * from {{ source('src_jan_pro_inside_sales_contactlist', 'contacts_knoxville') }}
+
+),
+
+renamed as (
+
+    select
+        list_source,
+        vertical,
+        disposition,
+        business_name,
+        business_phone,
+        address,
+        gk_name,
+        dm_name,
+        dm_title,
+        dm_phone,
+        direct_phone,
+        dm_email,
+        notes,
+        geo,
+        business_type,
+        business_size,
+        current_cleaning,
+        contract_expiration,
+        _1st_contact,
+        next_contact,
+        latest_contact,
+        index
+
+    from source
+
+)
+
+select * from renamed
