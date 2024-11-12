89c452941b912798024e4e000fef5c852d07410e
Concinnity Digital <concinnity.digital@gmail.com>
2024-11-12 11:00:16 +0000

    Fast Fleet Sales: added Nov 2024

---
+++ models/fast_fleet/staging/fast_fleet_sales/stg_fast_fleet_sales__2024_11.sql
@@ -0,0 +1,26 @@
+with source as (
+    select * from {{ source("src_fast_fleet_sales","2024_11") }}
+),
+renamed as (
+    select
+        Date,
+        Customer,
+        Amount,
+        Labor_Cost,
+        Job_Supplies,
+        Gross_Revenue,
+        cast(GP_Margin as string) as GP_Margin,
+        Service_Type,
+        Classification,
+        Job__ as Job,
+        Truck__ as Truck,
+        Invoice__ as Invoice,
+        Payment_Method,
+        Shift,
+        initcap(trim(Lead_Source)) as Lead_Source,
+        Notes
+    from source
+    where Date is not null
+)
+
+select * from renamed
