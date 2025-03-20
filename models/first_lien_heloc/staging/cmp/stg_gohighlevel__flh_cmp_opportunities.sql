422409e9d19b8b0fa18179030b75bd0315812644
Concinnity Digital <concinnity.digital@gmail.com>
2025-03-20 00:49:42 +0000

    FLH: updated pipeline opportunities

--- models/first_lien_heloc/staging/cmp/stg_gohighlevel__flh_cmp_opportunities.sql
+++ models/first_lien_heloc/staging/cmp/stg_gohighlevel__flh_cmp_opportunities.sql
@@ -11,16 +11,16 @@ renamed as (
     select
         string_field_0 as contact_id,
         string_field_1 as contactname,
-        string_field_2 as opportuniy_id,
-        string_field_4 as pipeline_name,
-        string_field_5 as stage_name,
-        string_field_6 as opportunity_assignedto,
-        string_field_7 as status,
-        string_field_8 as ambassador,
-        string_field_9 as child_ambassador,
+        string_field_2 as opportuniy_name,
+        string_field_3 as pipeline_name,
+        string_field_4 as stage_name,
+        string_field_5 as opportunity_assignedto,
+        string_field_6 as status,
+        string_field_7 as ambassador,
+        string_field_8 as child_ambassador,
         --cast(string_field_10 as date) as datecreated,
-        PARSE_DATE('%m/%d/%Y', string_field_10) as datecreated,
-        string_field_11 as dateupdated
+        PARSE_DATE('%m/%d/%Y', string_field_9) as datecreated,
+        string_field_10 as dateupdated
 
     from source
 
