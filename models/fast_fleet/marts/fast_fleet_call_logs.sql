402147df54f67dab57f60c37df07f8de1aecef22
Concinnity Digital <concinnity.digital@gmail.com>
2025-06-02 22:08:18 +0000

    Fast Fleet Sales: added june 2026

---
+++ models/fast_fleet/marts/fast_fleet_call_logs.sql
@@ -0,0 +1,18 @@
+with source as (
+    select * from {{ source("src_fast_fleet_call_logs","yr_2024") }}
+),
+renamed as (
+    select
+        date_started as Date,
+        operator_name,
+        disposition,
+        subdisposition_1,
+        direction,
+        external_number,
+        internal_number,
+        recording_url,
+    from source
+    where date_started is not null
+)
+
+select * from renamed
